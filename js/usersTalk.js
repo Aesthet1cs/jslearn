888d7bf7dbf05f0e6fa01ce20da63d003b1ff6a1
Konstantin <KonstantinNes@gmail.com>
2020-07-15 17:38:11 +0300

    first commit

---
+++ js/usersTalk.js
@@ -0,0 +1,11 @@
+"use strict"
+
+const answers = [];
+
+answer[0] = prompt('Как ваше имя', '');
+answer[1] = prompt('Как ваше Фамилия', '');
+answer[2] = prompt('Сколько лет тварь', '');
+
+document.write(answers);
+
+
