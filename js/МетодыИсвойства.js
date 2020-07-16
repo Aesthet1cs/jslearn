8ea37c749ad9c8c172a719d240be602233607320
Konstantin <KonstantinNes@gmail.com>
2020-07-16 17:57:30 +0300

    Третье задание с функциями

---
+++ js/МетодыИсвойства.js
@@ -0,0 +1,25 @@
+const arr = [ 1, 2, 4 ];
+
+console.log(arr.length);
+
+const str = "test";
+console.log(str);
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+
+const fruit = "some fruit";
+
+console.log(fruit.indexOf("fruit"));
+
+const logg = "Hello world";
+
+console.log(logg.slice(6, 11));
+console.log(logg.substring(6, 11));
+console.log(logg.substr(6, 5));
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = "12.2px";
+console.log(parseInt(test));
+console.log(parseFloat(test));
--- js/функции.js
+++ js/функции.js
@@ -1,1 +1,20 @@
 "use strict"
+
+function ret() {
+    let num = 50;
+
+    //
+
+    return num;
+}
+
+const anotherNum = ret();
+
+
+const logger = function() {
+    console.log('Hello');
+};
+
+logger();
+
+const calc = (a, b) => { return a + b };
